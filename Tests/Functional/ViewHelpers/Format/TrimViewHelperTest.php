60ebda62fc4e319bbe3b5e9e41ff4c529d189859
Roman Büchler <info@buechler.pro>
2021-11-22 17:13:22 +0200

    [TASK] Add format.trim vh

---
+++ Tests/Functional/ViewHelpers/Format/TrimViewHelperTest.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the composer package buepro/typo3-pvh.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Buepro\Pvh\Tests\Functional\ViewHelpers\Format;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\View\StandaloneView;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+class TrimViewHelperTest extends FunctionalTestCase
+{
+    private const TEMPLATE_PATH = 'EXT:pvh/Tests/Functional/ViewHelpers/Format/Fixtures/Trim.html';
+
+    /**
+     * @var bool Speed up this test case, it needs no database
+     */
+    protected $initializeDatabase = false;
+
+    /**
+     * @var array
+     */
+    protected $testExtensionsToLoad = [
+        'typo3conf/ext/pvh',
+    ];
+
+    public function renderDataProvider(): array
+    {
+        return [
+            'trim without characters' => ['  trimmed ', null, 'trimmed'],
+            'trim specific character 1' => [ 'ztrimmedy', 'zy', 'trimmed'],
+            'trim specific character 2' => [ 'ztzrimmeydy', 'zy', 'tzrimmeyd'],
+        ];
+    }
+
+    /**
+     * @dataProvider renderDataProvider
+     * @test
+     */
+    public function render(string $content, ?string $characters, string $expected): void
+    {
+        $view = GeneralUtility::makeInstance(StandaloneView::class);
+        $view->setTemplatePathAndFilename(self::TEMPLATE_PATH);
+        $view->assign('content', $content);
+        if ($characters !== null) {
+            $view->assign('characters', $characters);
+        }
+        $html = $view->render();
+        $xml = new \SimpleXMLElement($html);
+        $id = $characters !== null ? 'characters' : 'no-characters';
+        [$node] = $xml->xpath('//span[@id="' . $id . '"]');
+        $actual = trim((string)$node);
+        self::assertSame($expected, $actual);
+    }
+}
---
+++ ext_emconf.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the composer package buepro/typo3-pvh.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'PVH: Pizpalue View Helpers',
+    'description' => 'A collection of view helpers used by pizpalue. Contains copies from vhs view helpers by Claus Due.',
+    'category' => 'misc',
+    'version' => '1.0.0-dev',
+    'state' => 'stable',
+    'author' => 'Roman Büchler',
+    'author_email' => 'rb@buechler.pro',
+    'constraints' => [
+        'depends' => [
+            'php' => '7.3.0-8.0.99',
+            'typo3' => '10.4.0 - 11.5.99',
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Buepro\\Pvh\\' => 'Classes/'
+        ],
+    ],
+];
